9da6b2c5400884da4285e392018df90598e74372
meigoc <woercraftofficial@mail.ru>
2025-08-23 11:07:48 +0300

    Simplified import command

--- src/main/java/meigo/denizen/reflect/commands/ImportCommand.java
+++ src/main/java/meigo/denizen/reflect/commands/ImportCommand.java
@@ -1,6 +1,7 @@
 package meigo.denizen.reflect.commands;
 
 import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
+import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
@@ -17,14 +18,14 @@ public class ImportCommand extends AbstractCommand {
 
     public ImportCommand() {
         setName("import");
-        setSyntax("import class:<class_name> [constructor:<param>|...] [as:<definition_name>]");
+        setSyntax("import [<class_name>] [constructor:<param>|...] [as:<definition_name>]");
         setRequiredArguments(1, 3);
         isProcedural = false;
     }
 
     // <--[command]
     // @Name import
-    // @Syntax import class:<class_name> [constructor:<param>|...] [as:<definition_name>]
+    // @Syntax import [<class_name>] [constructor:<param>|...] [as:<definition_name>]
     // @Required 1
     // @Maximum 3
     // @Short Creates a new Java object instance or gets a static class reference.
@@ -34,7 +35,7 @@ public class ImportCommand extends AbstractCommand {
     // This command is the entry point for Java reflection in Denizen.
     // It allows you to create an instance of a Java class or get a reference to the class itself for static access.
     //
-    // The 'class' argument is required and must be the fully qualified name of the class (e.g., "java.util.ArrayList").
+    // The first argument is required and must be the fully qualified name of the class (e.g., "java.util.ArrayList").
     // Access to classes is restricted by the 'allowed-packages' list in the config.yml for security.
     //
     // The 'constructor' argument is optional. If provided, the command will attempt to create a new instance
@@ -49,27 +50,47 @@ public class ImportCommand extends AbstractCommand {
     //
     // @Usage
     // Use to create a new ArrayList and save it to the definition 'my_list'.
-    // - reflect class:java.util.ArrayList as:my_list
+    // - import java.util.ArrayList as:my_list
     //
     // @Usage
     // Use to get a static reference to the 'java.lang.System' class.
-    // - reflect class:java.lang.System as:system
+    // - import java.lang.System as:system
     //
     // @Usage
     // Use to create a new 'java.awt.Point' object with a specific constructor.
-    // - reflect class:java.awt.Point constructor:10|20 as:my_point
+    // - import java.awt.Point constructor:10|20 as:my_point
     // -->
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
+        for (Argument arg : scriptEntry) {
+            if (!scriptEntry.hasObject("class_name")
+                    && !arg.matchesPrefix("constructor")
+                    && !arg.matchesPrefix("as")) {
+                scriptEntry.addObject("class_name", arg.getRawElement());
+            }
+            else if (!scriptEntry.hasObject("constructor")
+                    && arg.matchesPrefix("constructor")) {
+                scriptEntry.addObject("constructor", arg.asType(ListTag.class));
+            }
+            else if (!scriptEntry.hasObject("as")
+                    && arg.matchesPrefix("as")) {
+                scriptEntry.addObject("as", arg.asElement());
+            }
+            else {
+                arg.reportUnhandled();
+            }
+        }
+        if (!scriptEntry.hasObject("class_name")) {
+            throw new InvalidArgumentsException("Missing class name argument!");
+        }
     }
 
     @Override
     public void execute(ScriptEntry scriptEntry) {
-        ElementTag className = scriptEntry.requiredArgForPrefixAsElement("class");
-        ListTag constructorArgs = scriptEntry.argForPrefix("constructor", ListTag.class, true);
-        ElementTag defName = scriptEntry.argForPrefixAsElement("as", null);
+        ElementTag className = scriptEntry.getElement("class_name");
+        ListTag constructorArgs = (ListTag) scriptEntry.getObject("constructor");
+        ElementTag defName = scriptEntry.getElement("as");
 
         if (scriptEntry.dbCallShouldDebug()) {
             Debug.report(scriptEntry, getName(),
@@ -79,8 +100,7 @@ public class ImportCommand extends AbstractCommand {
         }
 
         // If 'constructor' argument is not present at all, we want a static class reference.
-        // The modern way to check for argument presence is to see if its value is null.
-        if (scriptEntry.argForPrefix("constructor") == null) {
+        if (!scriptEntry.hasObject("constructor")) {
             Class<?> staticClass = ReflectionHandler.getClass(className.asString(), scriptEntry.getContext());
             if (staticClass != null) {
                 JavaObjectTag classObject = new JavaObjectTag(staticClass);
