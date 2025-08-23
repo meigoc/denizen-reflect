37960542cb15d9f61af364a9254be54f5e193d3c
meigoc <woercraftofficial@mail.ru>
2025-08-23 11:46:31 +0300

    Add support JavaObjectTag in constructor

--- src/main/java/meigo/denizen/reflect/commands/ImportCommand.java
+++ src/main/java/meigo/denizen/reflect/commands/ImportCommand.java
@@ -13,6 +13,7 @@ import meigo.denizen.reflect.util.ReflectionHandler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ImportCommand extends AbstractCommand {
 
@@ -39,7 +40,8 @@ public class ImportCommand extends AbstractCommand {
     // Access to classes is restricted by the 'allowed-packages' list in the config.yml for security.
     //
     // The 'constructor' argument is optional. If provided, the command will attempt to create a new instance
-    // of the class by finding a constructor that matches the provided parameters. Parameters should be a ListTag.
+    // of the class by finding a constructor that matches the provided parameters. Parameters can be any ObjectTag,
+    // including typed arguments like 'int#42'.
     // If omitted, the command will attempt to use the default no-argument constructor. If no constructor argument is given
     // at all, the command returns a static reference to the class itself, which can be used for static method/field access.
     //
@@ -58,28 +60,17 @@ public class ImportCommand extends AbstractCommand {
     //
     // @Usage
     // Use to create a new 'java.awt.Point' object with a specific constructor.
-    // - import java.awt.Point constructor:10|20 as:my_point
+    // - import java.awt.Point constructor:int#10|int#20 as:my_point
     // -->
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
         for (Argument arg : scriptEntry) {
             if (!scriptEntry.hasObject("class_name")
-                    && !arg.matchesPrefix("constructor")
-                    && !arg.matchesPrefix("as")) {
+                    && !arg.matchesPrefix("constructor", "as")) {
                 scriptEntry.addObject("class_name", arg.getRawElement());
             }
-            else if (!scriptEntry.hasObject("constructor")
-                    && arg.matchesPrefix("constructor")) {
-                scriptEntry.addObject("constructor", arg.asType(ListTag.class));
-            }
-            else if (!scriptEntry.hasObject("as")
-                    && arg.matchesPrefix("as")) {
-                scriptEntry.addObject("as", arg.asElement());
-            }
-            else {
-                arg.reportUnhandled();
-            }
+            // Other arguments are handled in execute
         }
         if (!scriptEntry.hasObject("class_name")) {
             throw new InvalidArgumentsException("Missing class name argument!");
@@ -89,8 +80,8 @@ public class ImportCommand extends AbstractCommand {
     @Override
     public void execute(ScriptEntry scriptEntry) {
         ElementTag className = scriptEntry.getElement("class_name");
-        ListTag constructorArgs = (ListTag) scriptEntry.getObject("constructor");
-        ElementTag defName = scriptEntry.getElement("as");
+        ListTag constructorArgs = scriptEntry.argForPrefix("constructor", ListTag.class, true);
+        ElementTag defName = scriptEntry.argForPrefixAsElement("as", null);
 
         if (scriptEntry.dbCallShouldDebug()) {
             Debug.report(scriptEntry, getName(),
@@ -100,7 +91,7 @@ public class ImportCommand extends AbstractCommand {
         }
 
         // If 'constructor' argument is not present at all, we want a static class reference.
-        if (!scriptEntry.hasObject("constructor")) {
+        if (constructorArgs == null && scriptEntry.argForPrefix("constructor") == null) {
             Class<?> staticClass = ReflectionHandler.getClass(className.asString(), scriptEntry.getContext());
             if (staticClass != null) {
                 JavaObjectTag classObject = new JavaObjectTag(staticClass);
@@ -113,7 +104,7 @@ public class ImportCommand extends AbstractCommand {
         }
 
         // If 'constructor' argument is present (even if empty), create an instance.
-        List<ObjectTag> params = constructorArgs != null ? constructorArgs.objectForms : new ArrayList<>();
+        List<ObjectTag> params = convertConstructorArgs(constructorArgs, scriptEntry);
         Object newInstance = ReflectionHandler.construct(className.asString(), params, scriptEntry.getContext());
 
         if (newInstance != null) {
@@ -123,5 +114,61 @@ public class ImportCommand extends AbstractCommand {
                 scriptEntry.getResidingQueue().addDefinition(defName.asString(), newObject);
             }
         }
+    }
+
+    private List<ObjectTag> convertConstructorArgs(ListTag args, ScriptEntry scriptEntry) {
+        if (args == null) {
+            return new ArrayList<>();
+        }
+        List<ObjectTag> convertedArgs = new ArrayList<>();
+        for (ObjectTag arg : args.objectForms) {
+            String argStr = arg.toString();
+            int atIndex = argStr.indexOf('#');
+            if (atIndex > 0) {
+                String typeName = argStr.substring(0, atIndex);
+                String value = argStr.substring(atIndex + 1);
+                ObjectTag typedArg = createTypedArgument(typeName, value, scriptEntry);
+                if (typedArg != null) {
+                    convertedArgs.add(typedArg);
+                    continue;
+                }
+            }
+            convertedArgs.add(arg);
+        }
+        return convertedArgs;
+    }
+
+    private ObjectTag createTypedArgument(String typeName, String value, ScriptEntry scriptEntry) {
+        try {
+            switch (typeName.toLowerCase(Locale.ENGLISH)) {
+                case "int":
+                case "integer":
+                    return new ElementTag(Integer.parseInt(value));
+                case "long":
+                    return new ElementTag(Long.parseLong(value));
+                case "float":
+                    return new ElementTag(Float.parseFloat(value));
+                case "double":
+                    return new ElementTag(Double.parseDouble(value));
+                case "boolean":
+                    return new ElementTag(Boolean.parseBoolean(value));
+                case "byte":
+                    return new ElementTag(Byte.parseByte(value));
+                case "short":
+                    return new ElementTag(Short.parseShort(value));
+                case "string":
+                case "java.lang.string":
+                    return new ElementTag(value);
+                default:
+                    Class<?> clazz = ReflectionHandler.getClass(typeName, scriptEntry.getContext());
+                    if (clazz != null) {
+                        return new ElementTag(value);
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            Debug.echoError(scriptEntry, "Failed to convert argument '" + value + "' to type '" + typeName + "': " + e.getMessage());
+        }
+        return null;
     }
 }
