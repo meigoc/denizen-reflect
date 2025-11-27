61a731553d820de873d4027e445ca510ad147d95
loyslow <gagagaopo@gmail.com>
2025-11-27 22:05:13 +0200

    new stable proxy system

--- src/main/java/meigo/denizen/reflect/commands/ProxyCommand.java
+++ src/main/java/meigo/denizen/reflect/commands/ProxyCommand.java
@@ -1,137 +1,158 @@
 package meigo.denizen.reflect.commands;
 
-import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
-import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.ObjectTag;
-import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.core.JavaReflectedObjectTag;
-import com.denizenscript.denizencore.objects.core.ListTag;
-import com.denizenscript.denizencore.objects.core.MapTag;
+import com.denizenscript.denizencore.objects.core.*;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
-import com.denizenscript.denizencore.scripts.ScriptEntry.ArgumentIterator;
+import com.denizenscript.denizencore.scripts.ScriptEntryData;
+import com.denizenscript.denizencore.scripts.ScriptRegistry;
 import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
+import com.denizenscript.denizencore.scripts.commands.generator.ArgLinear;
+import com.denizenscript.denizencore.scripts.commands.generator.ArgName;
+import com.denizenscript.denizencore.scripts.commands.generator.ArgPrefixed;
+import com.denizenscript.denizencore.scripts.containers.ScriptContainer;
+import com.denizenscript.denizencore.scripts.queues.core.InstantQueue;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
-import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import meigo.denizen.reflect.util.DenizenProxyHandler;
+import com.denizenscript.denizencore.utilities.text.StringHolder;
 import meigo.denizen.reflect.util.LibraryLoader;
+
+import java.lang.reflect.*;
+import java.util.*;
 
 public class ProxyCommand extends AbstractCommand {
 
-    // @Plugin denizen-reflect
     public ProxyCommand() {
-        this.setName("proxy");
-        this.setSyntax("proxy [<interfaces>|...] [using:<handler_map>/<section_name>/<task_script>] [as:<name>]");
-        this.setRequiredArguments(2, 3);
-        this.isProcedural = false;
+        setName("proxy");
+        setSyntax("proxy [<interfaces>|...] using:<map> as:<name>");
+        setRequiredArguments(3, 3);
+        autoCompile();
     }
 
-    // <--[command]
-    // @Name Proxy
-    // @Syntax proxy [<interfaces>|...] [using:<map>] [as:<name>]
-    // @Required 2
-    // @Maximum 3
-    // @Short Creates a proxy for given interfaces.
-    // @Group denizen-reflect
-    //
-    // @Description
-    // Creates a new proxy object using the specified interfaces.
-    //
-    // @Usage
-    // Use to create proxy of Runnable.
-    // - proxy java.lang.Runnable using:[run=task]g
-    // -->
+    @SuppressWarnings("unused")
+    public static void autoExecute(
+            ScriptEntry entry,
+            @ArgName("interfaces") @ArgLinear ListTag interfaces,
+            @ArgName("using") @ArgPrefixed MapTag using,
+            @ArgName("as") @ArgPrefixed String name) {
 
-    public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-        ArgumentIterator var2 = scriptEntry.iterator();
-
-        while(true) {
-            while(var2.hasNext()) {
-                Argument arg = (Argument)var2.next();
-                if (!scriptEntry.hasObject("interfaces") && !arg.matchesPrefix(new String[]{"using", "as"})) {
-                    scriptEntry.addObject("interfaces", arg.asType(ListTag.class));
-                } else if (arg.matchesPrefix("using")) {
-                    scriptEntry.addObject("handler_object", arg.object);
-                } else if (arg.matchesPrefix("as")) {
-                    scriptEntry.addObject("as", arg.asElement());
+        List<Class<?>> interfaceList = new ArrayList<>();
+        for (String interfaceName : interfaces) {
+            try {
+                Class<?> clazz = Class.forName(interfaceName, true, LibraryLoader.getClassLoader());
+                if (clazz.isInterface()) {
+                    interfaceList.add(clazz);
                 } else {
-                    arg.reportUnhandled();
+                    Debug.echoError("Class is not an interface: " + interfaceName);
                 }
+            } catch (ClassNotFoundException e) {
+                Debug.echoError("Interface not found: " + interfaceName);
             }
+        }
 
-            if (!scriptEntry.hasObject("interfaces")) {
-                throw new InvalidArgumentsException("Must specify interface classes!");
-            }
-
-            if (!scriptEntry.hasObject("handler_object")) {
-                throw new InvalidArgumentsException("Must specify handlers with 'using' argument!");
-            }
-
+        if (interfaceList.isEmpty()) {
+            Debug.echoError("No valid interfaces provided for proxy.");
             return;
         }
+
+        Map<String, ObjectTag> scriptHandlers = new HashMap<>();
+        for (Map.Entry<StringHolder, ObjectTag> mapEntry : using.map.entrySet()) {
+            scriptHandlers.put(mapEntry.getKey().low, mapEntry.getValue());
+        }
+
+        Object proxyInstance = Proxy.newProxyInstance(
+                LibraryLoader.getClassLoader(),
+                interfaceList.toArray(new Class[0]),
+                new DenizenInvocationHandler(scriptHandlers, entry)
+        );
+
+        entry.getResidingQueue().addDefinition(name, new JavaReflectedObjectTag(proxyInstance));
     }
 
-    public void execute(ScriptEntry scriptEntry) {
-        ListTag interfacesList = (ListTag)scriptEntry.getObjectTag("interfaces");
-        ObjectTag handlerObject = (ObjectTag)scriptEntry.getObjectTag("handler_object");
-        ElementTag asName = scriptEntry.getElement("as");
-        MapTag handlers = null;
-        ElementTag sectionOrTaskHandler = null;
-        if (handlerObject instanceof MapTag) {
-            handlers = (MapTag)handlerObject;
-        } else {
-            if (!(handlerObject instanceof ElementTag)) {
-                Debug.echoError(scriptEntry, "Invalid handler type specified! Must be a MapTag, section name, or task script.");
-                return;
+    private record DenizenInvocationHandler(Map<String, ObjectTag> scriptHandlers,
+                                            ScriptEntry creationEntry) implements InvocationHandler {
+
+            private static final Method INVOKE_DEFAULT;
+
+            static {
+                Method method;
+                try {
+                    method = InvocationHandler.class.getDeclaredMethod("invokeDefault", Object.class, Method.class, Object[].class);
+                    method.setAccessible(true);
+                } catch (NoSuchMethodException e) {
+                    method = null;
+                }
+                INVOKE_DEFAULT = method;
             }
 
-            sectionOrTaskHandler = (ElementTag)handlerObject;
-        }
+        @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                String methodName = method.getName();
+                String handlerKey = methodName.toLowerCase();
+                ObjectTag scriptName = scriptHandlers.get(handlerKey);
 
-        if (scriptEntry.dbCallShouldDebug()) {
-            Debug.report(scriptEntry, this.getName(), new Object[]{interfacesList, db("handler_object", handlerObject.identify()), asName});
-        }
+                if (scriptName == null) {
+                        if (method.isDefault() && INVOKE_DEFAULT != null) {
+                            return INVOKE_DEFAULT.invoke(this, proxy, method, args);
+                        }
 
-        try {
-            List<Class<?>> interfaceClasses = new ArrayList();
-            Iterator var8 = interfacesList.iterator();
+                    if (methodName.equals("toString") && method.getParameterCount() == 0) {
+                            return proxy.getClass().getName() + "@" + Integer.toHexString(proxy.hashCode());
+                        }
+                        if (methodName.equals("hashCode") && method.getParameterCount() == 0) {
+                            return System.identityHashCode(proxy);
+                        }
+                        if (methodName.equals("equals") && method.getParameterCount() == 1 && method.getParameterTypes()[0] == Object.class) {
+                            return proxy == args[0];
+                        }
 
-            while(var8.hasNext()) {
-                String interfaceName = (String)var8.next();
-                Class<?> ifaceClass = DenizenProxyHandler.getClassForName(interfaceName, scriptEntry);
-                if (ifaceClass == null) {
-                    Debug.echoError(scriptEntry, "Interface class not found: " + interfaceName);
-                    return;
+                        return null;
+                    }
+
+                int i = 0;
+
+                ListTag definitions;
+                ListTag argsList = new ListTag();
+                if (args != null) {
+                    for (Object arg : args) {
+                        argsList.addObject(new JavaReflectedObjectTag(arg));
+                    }
                 }
 
-                if (!ifaceClass.isInterface()) {
-                    Debug.echoError(scriptEntry, "Class is not an interface: " + interfaceName);
-                    return;
+                if (scriptName.getJavaObject() instanceof String) {
+                    ScriptContainer container = ScriptRegistry.getScriptContainer((String) scriptName.getJavaObject());
+                    definitions = new ListTag(container.getString("definitions"));
+                    ScriptEntryData data = creationEntry.entryData.clone();
+
+                    InstantQueue queue = new InstantQueue("PROXY_");
+                    queue.addEntries(container.getBaseEntries(data));
+                    for (String arg : argsList) {
+                        try {
+                            queue.addDefinition(definitions.get(i), arg);
+                        } catch (Exception e) {
+                            queue.addDefinition(String.valueOf(i + 1), arg);
+                        } i++;
+                    }
+                    queue.addDefinition("proxy", new JavaReflectedObjectTag(proxy));
+                    queue.addDefinition("method", new ElementTag(methodName));
+                    queue.start();
+                } else if (scriptName.getJavaObject() instanceof SectionCommand.Section section) {
+                    definitions = section.definitions;
+                    section.allDefinitions = new MapTag();
+                    section.allDefinitions.putAll(section.defMap);
+                    section.allDefinitions.putObject("proxy", new JavaReflectedObjectTag(proxy));
+                    section.allDefinitions.putObject("method", new ElementTag(methodName));
+                    for (ObjectTag arg : argsList.objectForms) {
+                        try {
+                            section.allDefinitions.putObject(definitions.get(i), arg);
+                        } catch (Exception e) {
+                            section.allDefinitions.putObject(String.valueOf(i + 1), arg);
+                        } i++;
+                    section.run();
+                    }
+                } else {
+                    Debug.echoError("Proxy handler refers to missing script: " + scriptName);
+                    return null;
                 }
-
-                interfaceClasses.add(ifaceClass);
+                return proxy;
             }
-
-            DenizenProxyHandler handler;
-            if (handlers != null) {
-                handler = new DenizenProxyHandler(handlers, scriptEntry.getContext());
-            } else {
-                handler = new DenizenProxyHandler(sectionOrTaskHandler, scriptEntry.getContext());
-            }
-
-            Object proxy = Proxy.newProxyInstance(LibraryLoader.getClassLoader(), (Class[])interfaceClasses.toArray(new Class[0]), handler);
-            JavaReflectedObjectTag proxyObject = new JavaReflectedObjectTag(proxy);
-            proxyObject.persist();
-            if (asName != null) {
-                scriptEntry.getResidingQueue().addDefinition(asName.asString(), proxyObject);
-            }
-        } catch (Exception var11) {
-            Debug.echoError(scriptEntry, "Failed to create proxy:");
-            Debug.echoError(scriptEntry, var11);
         }
-
-    }
 }
