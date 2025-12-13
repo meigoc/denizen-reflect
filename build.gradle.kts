73f209bad7e90132f79b0150bba621c718c11f95
loyslow <gagagaopo@gmail.com>
2025-12-13 18:35:30 +0200

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
 val buildNumber: String = System.getenv("BUILD_NUMBER") ?: project.property("BUILD_NUMBER") as String
 val buildDate: String = SimpleDateFormat("ddMMyyyy").format(Date())
-val pluginVersion = "1.0_Build${buildNumber}_${buildDate}"
+val pluginVersion = "2.2"
 
 group = "meigo"
 version = pluginVersion
